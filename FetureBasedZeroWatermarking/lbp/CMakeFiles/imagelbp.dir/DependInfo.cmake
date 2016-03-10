401e634586a64f85e6aab5aa32436c6eaf9ced2a
thanhtm <taminhjp@gmail.com>
2016-03-10 18:18:19 +0700

    Init project cho watermarking

---
+++ FetureBasedZeroWatermarking/lbp/CMakeFiles/imagelbp.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/framgia/Downloads/opencv-master/lbp/histogram.cpp" "/Users/framgia/Downloads/opencv-master/lbp/CMakeFiles/imagelbp.dir/histogram.cpp.o"
+  "/Users/framgia/Downloads/opencv-master/lbp/imagelbp.cpp" "/Users/framgia/Downloads/opencv-master/lbp/CMakeFiles/imagelbp.dir/imagelbp.cpp.o"
+  "/Users/framgia/Downloads/opencv-master/lbp/lbp.cpp" "/Users/framgia/Downloads/opencv-master/lbp/CMakeFiles/imagelbp.dir/lbp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ FetureBasedZeroWatermarking/lda/CMakeFiles/lda.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/framgia/Downloads/opencv-master/lda/src/fisherfaces.cpp" "/Users/framgia/Downloads/opencv-master/lda/CMakeFiles/lda.dir/src/fisherfaces.cpp.o"
+  "/Users/framgia/Downloads/opencv-master/lda/src/helper.cpp" "/Users/framgia/Downloads/opencv-master/lda/CMakeFiles/lda.dir/src/helper.cpp.o"
+  "/Users/framgia/Downloads/opencv-master/lda/src/main.cpp" "/Users/framgia/Downloads/opencv-master/lda/CMakeFiles/lda.dir/src/main.cpp.o"
+  "/Users/framgia/Downloads/opencv-master/lda/src/subspace.cpp" "/Users/framgia/Downloads/opencv-master/lda/CMakeFiles/lda.dir/src/subspace.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
